1c2084bb882d7fe3392481a6d87e7e50c25d11fa
Johann Dahm <jdahm@fastmail.com>
2015-10-14 12:48:00 -0400

    Add back org-present.

--- emacs/.emacs.d/lisp/setup-org.el
+++ emacs/.emacs.d/lisp/setup-org.el
@@ -54,10 +54,23 @@
 
 (setq org-latex-pdf-process (list "latexmk -lualatex -f %f"))
 
-(setq org-completion-use-ido t)
-
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((python . t)))
+
+(eval-after-load "org-present"
+  '(progn
+     (add-hook 'org-present-mode-hook
+               (lambda ()
+                 (org-present-big)
+                 (org-display-inline-images)
+                 (org-present-hide-cursor)
+                 (org-present-read-only)))
+     (add-hook 'org-present-mode-quit-hook
+               (lambda ()
+                 (org-present-small)
+                 (org-remove-inline-images)
+                 (org-present-show-cursor)
+                 (org-present-read-write)))))
 
 (provide 'setup-org)
