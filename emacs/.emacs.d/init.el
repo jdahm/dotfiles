b6e629b88c6ac1822d636e3c3a69cb5349fe8917
Johann Dahm <jdahm@fastmail.com>
2016-02-05 17:26:29 -0500

    Alignment.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -141,8 +141,8 @@
           (>= emacs-major-version 25))
   (setq package-archives
 	'(("melpa-stable" . "http://stable.melpa.org/packages/")
-          ("melpa" . "http://melpa.org/packages/")
-          ("gnu"   . "http://elpa.gnu.org/packages/")))
+          ("melpa"        . "http://melpa.org/packages/")
+          ("gnu"          . "http://elpa.gnu.org/packages/")))
   (package-initialize)
 
   (when (not package-archive-contents) (package-refresh-contents))
