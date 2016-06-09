8a927d9f56897c407b7082e5237b09a43ee8a372
Johann Dahm <jdahm@fastmail.com>
2016-06-09 09:11:22 -0400

    Emacs: Use string-equal to check for system-type.

--- emacs/.emacs.d/lisp/dired.el
+++ emacs/.emacs.d/lisp/dired.el
@@ -2,16 +2,16 @@
   "Open a GUI file manager at (dired-current-directory) using
 xdg-open."
   (interactive)
-    (if (eq (system-type) "darwin")
-	(call-process "open" nil 0 nil (dired-current-directory))
-      (call-process "xdg-open" nil 0 nil (dired-current-directory))))
+  (if (string-equal system-type "darwin")
+      (call-process "open" nil 0 nil (dired-current-directory))
+    (call-process "xdg-open" nil 0 nil (dired-current-directory))))
 
 (defun dired-open-file ()
   "In dired, open the file named on this line."
   (interactive)
   (let* ((file (dired-get-filename nil t)))
     (message "Opening %s..." file)
-    (if (eq (system-type) "darwin")
+    (if (string-equal system-type "darwin")
 	(call-process "open" nil 0 nil file)
       (call-process "xdg-open" nil 0 nil file))
     (message "Opening %s done" file)))
