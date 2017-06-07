9fcc9901233573ea9ce2cf83d746be1d00d5276f
Johann Dahm <johann.dahm@gmail.com>
2017-06-07 11:41:29 -0700

    Revert "[Emacs] Remove no-littering."
    This reverts commit ceeab6d67188394ce96925f1d825bd0f8f141e80.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -31,6 +31,10 @@
 ;; Enable y/n answers
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+;; Keep things clean
+(require-package 'no-littering)
+(require 'no-littering)
+
 ;; Backups - better to be safe
 (setq backup-by-copying t
       delete-old-versions t
