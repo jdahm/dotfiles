f16ee903e7c6be66e0ee5b209928e90784e96ff3
Johann Dahm <johann.dahm@gmail.com>
2022-08-13 09:59:20 -0700

    Update

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,17 +1,12 @@
 set -U fish_greeting
 
+# Homebrew
 if test -f ~/.config/brew/path
     set PATH (cat ~/.config/brew/path)/bin $PATH
+    alias brew-update="brew bundle --cleanup --global"
 end
 
-set PATH $HOME/bin $HOME/go/bin $PATH
-
-if status is-interactive
-    alias vim=nvim
-    alias vi=nvim
-    set -x EDITOR nvim
-    set -x VISUAL nvim
-
+if command -qs brew
     if test -d (brew --prefix)"/share/fish/completions"
         set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
     end
@@ -19,22 +14,36 @@ if status is-interactive
     if test -d (brew --prefix)"/share/fish/vendor_completions.d"
        set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
     end
-    # Commands to run in interactive sessions can go here
-
-    # Hydro colors
-    set --global hydro_color_git green
-    set --global hydro_color_duration yellow
-    set --global hydro_color_prompt blue
-    set --global hydro_color_pwd magenta
-
-    alias chezmoi-cd="cd (chezmoi source-path)"
-    alias brew-update="brew bundle --cleanup --global"
-
-    alias s="kitty +kitten ssh"
-
-    set fish_key_bindings fish_user_key_bindings
 end
 
+# Local bin and lang executables
+set PATH $HOME/bin $HOME/go/bin $PATH
+
+# Set vi
+set -Ux EDITOR nvim
+set -Ux VISUAL nvim
+
+# set fish_key_bindings fish_user_key_bindings
+
+# Hydro colors
+set --global hydro_color_git green
+set --global hydro_color_duration yellow
+set --global hydro_color_prompt blue
+set --global hydro_color_pwd magenta
+
+# Direnv
 direnv hook fish | source
-# trigger direnv at prompt, and on every arrow-based directory change (default)
-set -g direnv_fish_mode eval_on_arrow
+
+alias chezmoi-cd="cd (chezmoi source-path)"
+
+# Kitty
+alias s="kitty +kitten ssh"
+alias icat="kitty +kitten icat"
+alias s="kitty +kitten ssh"
+alias hg="kitty +kitten hyperlinked_grep"
+
+if test -n "$KITTY_WINDOW_ID"
+    alias e="edit-in-kitty"
+else
+    alias e=$EDITOR
+end
--- dot_config/private_fish/functions/fish_user_key_bindings.fish
+++ dot_config/private_fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,10 @@
 function fish_user_key_bindings
   fish_vi_key_bindings
   bind -M insert -m default jk backward-char force-repaint
+
+  # for accepting autosuggestions in vi mode
+  # https://github.com/fish-shell/fish-shell/issues/3541#issuecomment-260001906
+  for mode in insert default visual
+    bind -M $mode \cf forward-char
+  end
 end
