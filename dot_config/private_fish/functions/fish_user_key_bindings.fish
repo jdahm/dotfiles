0b444710d9270510bc16b8653871ce15794e095b
Johann Dahm <johann.dahm@gmail.com>
2022-08-18 21:10:39 -0700

    Activate history search with '/'

--- dot_config/private_fish/functions/fish_user_key_bindings.fish
+++ dot_config/private_fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,10 @@
+function reverse_history_search
+  history | fzf --no-sort | read -l command
+  if test $command
+    commandline -rb $command
+  end
+end
+
 function fish_user_key_bindings
   fish_vi_key_bindings
   bind -M insert -m default jk backward-char force-repaint
@@ -7,4 +14,6 @@ function fish_user_key_bindings
   for mode in insert default visual
     bind -M $mode \cf forward-char
   end
+
+  bind -M default / reverse_history_search
 end
