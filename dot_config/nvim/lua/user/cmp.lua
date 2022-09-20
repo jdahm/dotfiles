bca63d0bba389f5f6152418a982fce76d0ad86a1
Johann Dahm <johann.dahm@gmail.com>
2022-09-19 22:28:00 -0700

    Update dot_config/nvim/lua/user/cmp.lua

--- dot_config/nvim/lua/user/cmp.lua
+++ dot_config/nvim/lua/user/cmp.lua
@@ -85,12 +85,3 @@ cmp.setup({
     end, { 'i', 's' }),
   },
 })
-
--- require("null-ls").setup({
---     sources = {
---         require("null-ls").builtins.formatting.stylua,
---         require("null-ls").builtins.diagnostics.eslint,
---         require("null-ls").builtins.completion.spell,
---     },
---     on_attach = on_attach,
--- })
