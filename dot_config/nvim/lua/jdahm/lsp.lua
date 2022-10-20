baf5996428d0a52a03f0841c2022dc1a60169b0a
Johann Dahm <johann.dahm@gmail.com>
2022-10-20 09:34:33 -0700

    Update dot_config/nvim/lua/jdahm/lsp.lua

--- dot_config/nvim/lua/jdahm/lsp.lua
+++ dot_config/nvim/lua/jdahm/lsp.lua
@@ -118,7 +118,7 @@ function M.setup()
     for _, lsp in pairs(servers) do
         if lsp ~= "" then
             local setup = {
-                capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+                -- capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
                 on_attach = function(client, bufnr)
                     lsp_keybindings(client, bufnr)
                     if ({ pylsp = "", sumneko_lua = "" })[lsp] ~= nil then
