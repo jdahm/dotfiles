9f5adb8c7dab5273096dcb40143e04ab415626ac
Johann Dahm <johann.dahm@gmail.com>
2023-04-06 16:41:23 -0700

    Remove run_onchange_vim_init.sh

--- run_onchange_vim_init.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-vim -u NONE -c "helptags abolish/doc" -c q
-vim -u NONE -c "helptags unimpaired/doc" -c q
-vim -u NONE -c "helptags fugitive/doc" -c q
-vim -u NONE -c "helptags vim-rhubarb/doc" -c q
-vim -u NONE -c "helptags vim-surround/doc" -c q
-
