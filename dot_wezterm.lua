28a66298bdd2ea2fcab5e2e8c1045ead6d5b0c94
Johann Dahm <johann.dahm@gmail.com>
2024-06-26 22:53:26 -0700

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -11,5 +11,7 @@ config.scrollback_lines = 5000
 -- config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "INTEGRATED_BUTTONS"
 
+config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+
 -- and finally, return the configuration to wezterm
 return config
