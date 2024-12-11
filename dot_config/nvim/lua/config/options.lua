dd36fdc88c8202f6b95dd7ad80bd7aecf0bec3dc
Johann Dahm <johannd@allenai.org>
2024-12-10 22:18:44 -0800

    Update

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -6,3 +6,8 @@ vim.o.shell = "/opt/homebrew/bin/fish -l"
 vim.g.ai_cmp = true
 
 vim.opt.mouse = ""
+
+vim.opt.mousescroll = "ver:0,hor:0"
+
+-- Setting vim.opt.list = false will disable all whitespace characters.
+-- vim.opt.list = false
